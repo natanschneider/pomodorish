1d7c744bc7c04d5810670787d95daea7237d34e2
Natãn Schneider <natanschneider@protonmail.com>
2022-09-27 12:53:40 -0300

    start function working

--- timer.js
+++ timer.js
@@ -1,3 +1,30 @@
-function timer(time){
+let minutes = 25;
+let seconds = 0;
+let myInterval;
+
+window.onload = () =>{
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+}
+
+function start(){
+    seconds = 4;
+    minutes = 24;
+
+    let timerFunction = () => {
+        seconds = seconds - 1;
+
+        if (seconds === 0) {
+            minutes = minutes - 1;
+            seconds = 59;
+        }
+
+        document.getElementById('minutes').innerHTML = minutes;
+        document.getElementById('seconds').innerHTML = seconds;
+    }
+    myInterval = setInterval(timerFunction, 1000);
+}
+
+function stop(){
 
 }
