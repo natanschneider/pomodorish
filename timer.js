d412dd51cfb789751a9f7ec0d13e8c53e8b2011e
Natãn Schneider <natanschneider@protonmail.com>
2022-09-27 13:26:46 -0300

    stop function working

--- timer.js
+++ timer.js
@@ -1,24 +1,54 @@
 let minutes = 25;
 let seconds = 0;
 let myInterval;
+let Break = false;
+let onlyRunOnce = true;
 
 window.onload = () =>{
     document.getElementById('minutes').innerHTML = minutes;
     document.getElementById('seconds').innerHTML = seconds;
 }
 
+function check(){
+    if(onlyRunOnce === true){
+        onlyRunOnce = false;
+        start();
+    }else{
+        return false;
+    }
+}
+
 function start(){
-    seconds = 4;
-    minutes = 24;
+    if(Break === false){
+        seconds = 60;
+        minutes = 24;
+    }else if(Break === true){
+        seconds = 60;
+        minutes = 4;
+    }
 
     let timerFunction = () => {
         seconds = seconds - 1;
 
-        if (seconds === 0) {
+        if(seconds === 0){
             minutes = minutes - 1;
             seconds = 59;
         }
 
+        if(minutes === -1){
+            stop();
+            if(Break === true){
+                Break = false;
+                minutes = 25;
+                seconds = 0;
+                new Notification('Time to get back to work!');
+            }else if(Break === false){
+                Break = true;
+                minutes = 5;
+                seconds = 0;
+                new Notification('You deserve a break now!');
+            }
+        }
         document.getElementById('minutes').innerHTML = minutes;
         document.getElementById('seconds').innerHTML = seconds;
     }
@@ -26,5 +56,6 @@ function start(){
 }
 
 function stop(){
-
+    clearInterval(myInterval);
+    onlyRunOnce = true;
 }
