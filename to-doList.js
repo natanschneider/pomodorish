2f325b24341da3fa234dc0a7cc43826171273e86
Natãn Schneider <natanschneider@protonmail.com>
2022-09-29 22:23:52 -0300

    *feature* add tasks to HTML Body

--- to-doList.js
+++ to-doList.js
@@ -1,5 +1,29 @@
 function setItem(value){
-    let key = '1';
+    let key = 1;
 
-    localStorage.setItem(key, value);
+    localStorage.setItem(key.toString(), value);
+    getItem(key);
+    key++;
+}
+
+function getItem(key){
+    let item = localStorage.getItem(key);
+
+    let div = document.createElement('div');
+
+    div.id = 'taskItem';
+
+    let h4 = document.createElement('h4');
+    h4.textContent = item;
+    div.appendChild(h4);
+
+    document.body.appendChild(div);
+}
+
+function deleteItem(key){
+    localStorage.removeItem(key);
+}
+
+function editItem(){
+
 }
