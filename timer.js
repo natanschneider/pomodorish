97b2a2f014906f5e7f29c67e2860cb52d6cd10f9
Natãn Schneider <natanschneider@protonmail.com>
2022-10-01 20:26:42 -0300

    git problems

--- timer.js
+++ timer.js
@@ -6,62 +6,62 @@ let onlyRunOnce = true;
 let numBreak = 1;
 
 window.onload = () =>{
-    document.getElementById('minutes').innerHTML = minutes;
-    document.getElementById('seconds').innerHTML = seconds;
-
-    listItems();
+	document.getElementById('minutes').innerHTML = minutes;
+	document.getElementById('seconds').innerHTML = seconds;
+	
+	listItems();
 }
 
 function check(){
-    if(onlyRunOnce === true) {
-        onlyRunOnce = false;
-        start();
-    }
+	if(onlyRunOnce === true) {
+		onlyRunOnce = false;
+		start();
+	}
 }
 
 function start(){
-    if(Break === false){
-        seconds = 60;
-        minutes = 24;
-    }else if(Break === true && numBreak === 4){
-        seconds = 60;
-        minutes = 14;
-        numBreak = 0;
-    }else if(Break === true){
-        seconds = 60;
-        minutes = 4;
-        numBreak++;
-    }
-
-    let timerFunction = () => {
-        seconds = seconds - 1;
-
-        if(seconds === 0){
-            minutes = minutes - 1;
-            seconds = 59;
-        }
-
-        if(minutes === -1){
-            stop();
-            if(Break === true){
-                Break = false;
-                minutes = 25;
-                seconds = 0;
-                new Notification('Time to get back to work!');
-            }else if(Break === false){
-                Break = true;
-                minutes = 5;
-                seconds = 0;
-                new Notification('You deserve a break now!');
-            }
-        }
-        document.getElementById('minutes').innerHTML = minutes;
-        document.getElementById('seconds').innerHTML = seconds;
-    }
-    myInterval = setInterval(timerFunction, 1000);
+	if(Break === false){
+		seconds = 60;
+		minutes = 24;
+	}else if(Break === true && numBreak === 4){
+		seconds = 60;
+		minutes = 14;
+		numBreak = 0;
+	}else if(Break === true){
+		seconds = 60;
+		minutes = 4;
+		numBreak++;
+	}
+	
+	let timerFunction = () => {
+		seconds = seconds - 1;
+		
+		if(seconds === 0){
+			minutes = minutes - 1;
+			seconds = 59;
+		}
+		
+		if(minutes === -1){
+			stop();
+			if(Break === true){
+				Break = false;
+				minutes = 25;
+				seconds = 0;
+				new Notification('Time to get back to work!');
+			}else if(Break === false){
+				Break = true;
+				minutes = 5;
+				seconds = 0;
+				new Notification('You deserve a break now!');
+			}
+		}
+		document.getElementById('minutes').innerHTML = minutes;
+		document.getElementById('seconds').innerHTML = seconds;
+	}
+	myInterval = setInterval(timerFunction, 1000);
 }
 
 function stop(){
-    clearInterval(myInterval);
-    onlyRunOnce = true;
+	clearInterval(myInterval);
+	onlyRunOnce = true;
 }
--- to-doList.js
+++ to-doList.js
@@ -2,56 +2,56 @@ let num;
 let storageLength = localStorage.length;
 
 if(storageLength !== 0){
-    num = storageLength;
+	num = storageLength;
 }else{
-    num = 1;
+	num = 1;
 }
 
 function setItem(value){
-    let key = num;
-    localStorage.setItem(key.toString(), value);
-    getItem(key);
-    num++;
+	let key = num;
+	localStorage.setItem(key.toString(), value);
+	getItem(key);
+	num++;
 }
 
 function getItem(key){
-    let item = localStorage.getItem(key);
-
-    let div = document.createElement('div');
-
-    div.class = 'taskItem';
-    div.id = key;
-
-    let h4 = document.createElement('h4');
-    h4.textContent = item;
-
-    let deleteBtn = document.createElement('button');
-    deleteBtn.id = 'deleteTask';
-    deleteBtn.innerHTML = '<img src="images/cancel.png"  alt="" id="deleteImg"/>';
-    deleteBtn.onclick = function(){
-        deleteItem(key);
-    };
-
-    div.appendChild(h4);
-    div.appendChild(deleteBtn);
-    document.body.appendChild(div);
+	let item = localStorage.getItem(key);
+	
+	let div = document.createElement('div');
+	
+	div.class = 'taskItem';
+	div.id = key;
+	
+	let h4 = document.createElement('h4');
+	h4.textContent = item;
+	
+	let deleteBtn = document.createElement('button');
+	deleteBtn.id = 'deleteTask';
+	deleteBtn.innerHTML = '<img src="images/cancel.png"  alt="" id="deleteImg"/>';
+	deleteBtn.onclick = function(){
+		deleteItem(key);
+	};
+	
+	div.appendChild(h4);
+	div.appendChild(deleteBtn);
+	document.body.appendChild(div);
 }
 
 function deleteItem(key){
-    localStorage.removeItem(key);
-
-    let div = document.getElementById(key);
-    div.remove();
+	localStorage.removeItem(key);
+	
+	let div = document.getElementById(key);
+	div.remove();
 }
 
 function listItems(){
-    let i;
-    let numberOfItems = localStorage.length;
-    if(numberOfItems >= 1) {
-        for (i = 1; i <= numberOfItems; i++) {
-            getItem(i);
-        }
-    }else{
-        return false;
-    }
+	let i;
+	let numberOfItems = localStorage.length;
+	if(numberOfItems >= 1) {
+		for (i = 1; i <= numberOfItems; i++) {
+			getItem(i);
+		}
+	}else{
+		return false;
+	}
 }
